a2c498dbe624c15557f507007fa80a9856c5b787
CastellucciLuca <castellucciluca98@gmail.com>
2023-02-08 16:59:30 +0100

    Add current series banner, comyics section style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('main-app')
-<main>
-    <div class="container text-light">
-        <div class="row row-cols-6">
-            @foreach ($comicList as $comic)
-            <div class="col">
-                <div class="img-container w-100 h-60">
-                    <img src="{{$comic['thumb']}}" alt="" class="h-100 w-100">
-                </div>
-                <h3 class="fs-6 fw-bold text-uppercase">
-                    {{$comic['series']}}
-                </h3>
-            </div>
-            @endforeach
-            <div class="container d-flex justify-content-between">
-                    <button class="btn btn-primary">Load More</button>
-                </div>
+<main id="comics">
+    <section id="comics-card" class="position-relative container">
+
+        <div class="title-series-section">
+            <h2 class="fs-3 m-0">Current Series</h2>
         </div>
-    </div>
+        
+        <div class="text-light pt-5">
+            <div class="row row-cols-6">
+                @foreach ($comicsList as $comic)
+                <div class="col">
+                    <div class="img-container">
+                        <img src="{{$comic['thumb']}}" alt="" class="h-100 w-100">
+                    </div>
+                    <h3 class="fs-6 fw-bold text-uppercase">
+                        {{$comic['series']}}
+                    </h3>
+                </div>
+                @endforeach
+                <button class="btn btn-primary m-auto mb-5">Load More</button>
+            </div>
+        </div>
+        
+    </section>
 </main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comicsList = config('comics');
-    return view('home', ['comicList' => $comicsList]);
+    return view('home', ['comicsList' => $comicsList]);
 })->name('home');
